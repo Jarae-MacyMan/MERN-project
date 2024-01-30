7f33444ec5726863423507c6fe2aca2b501eb134
gitpushjoe <104105011+gitpushjoe@users.noreply.github.com>
2024-01-30 18:26:54 -0500

    create /patients/ API endpoint

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var patientsRouter = require('./routes/patients');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/patients', patientsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,6 +36,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(3000,function(){
+  console.log('Server is running on port 3000');
 });
 
 module.exports = app;
