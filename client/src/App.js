cd4283505f0c23e955d55df6713c4650b5a2c0e4
Eki <ekiuzams@gmail.com>
2024-02-04 10:56:22 -0500

    Adding Tailwind configs

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
+import "./App.css";
+import Homescreen from "./components/Homescreen";
 
-import { useApi } from './hooks/use-api';
+import { useApi } from "./hooks/use-api";
 
 function App() {
   const { response } = useApi();
 
   return (
+    // <div className="App bg-blue-500 text-white p-4">
+    //   <header className="App-header">
+    //     <p>{response}</p>
+    //   </header>
+    // </div>
     <div className="App">
-      <header className="App-header">
-        <p>
-          {response}
-        </p>
-      </header>
+      <Homescreen />
     </div>
   );
 }
